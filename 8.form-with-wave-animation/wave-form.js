564ab7ffb7e1b2c112b78d241d74eb11f44ee6d9
MaxwellYu <yuichiuyu1915@gmail.com>
2021-05-13 14:20:32 +0800

    Login form with wave animation

--- 8.form-with-wave-animation/wave-form.js
+++ 8.form-with-wave-animation/wave-form.js
@@ -2,6 +2,7 @@
 const labels = document.querySelectorAll('.form-control > label');
 
 labels.forEach((item) => {
+    // wrap text inside <span>.
     item.innerHTML = item.innerText
         .split("")
         .map((letter, idx) => `<span style='transition-delay:${idx*50}ms'>${letter}</span>`)
