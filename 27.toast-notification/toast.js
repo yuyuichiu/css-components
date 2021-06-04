90432aa8e9155bd5404b72b457b9cfa5cec1ac19
MaxwellYu <yuichiuyu1915@gmail.com>
2021-06-04 14:40:51 +0800

    Pop notifications that spawns on corner on-click

--- 27.toast-notification/toast.js
+++ 27.toast-notification/toast.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("button");
 const toastArea = document.getElementById("toasts");
-/* Custom list of messages to be chosen randomly */
+/* Custom list of silly messages to be chosen randomly */
 let msg = [
     "Hot singles in your area!",
     "This stock grows 100x in a year!!!",
@@ -10,17 +10,20 @@ let msg = [
     "Bill Gates saids to invest in this...",
     "You cannot believe this just happened..."
 ]
+let rainbowColors = ['#9400D3','#4B0082','#0000FF','#50DF33','#EEEE00',"#FF7F00","#FF0000"]
+
+// Onclick events
 button.addEventListener('click', showNotification);
 
 function showNotification(){
     let rand = Math.floor(Math.random() * msg.length);
-    let randRgb = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`
+    let randRgb = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
 
     // Generate notification with a random color
     let notification = document.createElement('div');
     notification.classList.add('toast');
     notification.style.color = randRgb;
-    notification.innerText = msg[rand];
+    notification.innerHTML = `${msg[rand]}<i class="fas fa-times close" onclick="removeNotification(this)"></i>`;
 
     // Show that notification for a while, then make it self-destruct
     toastArea.appendChild(notification);
@@ -28,4 +31,8 @@ function showNotification(){
     setTimeout(() => {
         notification.remove();
     },3000)
+}
+
+function removeNotification(obj){
+    obj.parentElement.remove();
 }
