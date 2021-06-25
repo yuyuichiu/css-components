bcfc470566eaa5d47ed26e9a0ffc4a4a18f13bf7
MaxwellYu <yuichiuyu1915@gmail.com>
2021-06-25 21:33:09 +0800

    Fetch API data with xmlHttpRequest

--- 37.xmlhttprequest/xml.js
+++ 37.xmlhttprequest/xml.js
@@ -13,14 +13,12 @@ function getData(){
     request.send(null);
 
     request.onload = () => {
-        console.log(request.response)
         updatePersonInfo(request.response)
         request.abort();
     }
 }
 
 function updatePersonInfo(data) {
-    console.log("hi");
     let result = data.results[0];
 
     output.innerHTML = "";
@@ -30,11 +28,26 @@ function updatePersonInfo(data) {
         <div class="name">${result.name.first + " " + result.name.last}</div>
         <div class="email">${result.email}</div>
     `;
+    generateBtn.innerHTML = "Generate Person";
+    generateBtn.disabled = false;
 }
 
 generateBtn.addEventListener('click', () => {
-    console.log('clicked');
+    generateBtn.innerHTML = "Generating...";
+    generateBtn.disabled = true;
     getData();
 });
 
 getData();
+
+
+// playing with fetch api with configs
+function fetchGov(){
+    let url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBRzIkLqS59M_6neW0HHrGfv_eFdrJqK2E"
+    fetch(url, {
+        method: 'POST',
+        // mode: 'no-cors'
+    })
+    .then(response => response.json())
+    .then(data => console.log({lat: data.location.lat, lng: data.location.lng}));
+}
